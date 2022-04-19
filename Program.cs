24b9b5c95f5d8301186900d589a46bef14a27181
CamLinh42 <93019854+CamLinh42@users.noreply.github.com>
2022-04-19 13:22:40 +0700

    BT Tính đa hình

--- Program.cs
+++ Program.cs
@@ -1,167 +1,125 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BTSV
+namespace DaHinh
 {
+    class DaGiac
+    {
+        int soCanh;
+
+        public DaGiac(int soCanh)
+        {
+            SoCanh = soCanh;
+        }
+
+        public int SoCanh { get => soCanh; set => soCanh = value; }
+
+        virtual public void Xuat()
+        {
+            Console.WriteLine("Day la da giac.");
+        }
+    }
+    class HinhVuong : DaGiac
+    {
+        public HinhVuong(int soCanh) : base(soCanh)
+        {
+            SoCanh = soCanh;
+        }
+
+        override public void Xuat()
+        {
+            Console.WriteLine("Co 4 canh bang nhau va 1 goc vuong.");
+        }
+    }
+    class TamGiac : DaGiac
+    {
+        public TamGiac(int soCanh) : base(soCanh)
+        {
+            SoCanh = soCanh;
+        }
+
+        override public void Xuat()
+        {
+            Console.WriteLine("Co 3 canh va tong 2 canh bat ki lon hon canh con lai.");
+        }
+    }
+    class HinhChuNhat : DaGiac
+    {
+        public HinhChuNhat(int soCanh) : base(soCanh)
+        {
+            SoCanh = soCanh;
+        }
+
+        override public void Xuat()
+        {
+            Console.WriteLine("Co 2 cap canh bang nhau va 1 goc vuong.");
+        }
+    }
+    class HinhTuGiac : DaGiac
+    {
+        public HinhTuGiac(int soCanh) : base(soCanh)
+        {
+            SoCanh = soCanh;
+        }
+
+        override public void Xuat()
+        {
+            Console.WriteLine("Co 4 canh.");
+        }
+    }
+    class HinhBinhHanh : DaGiac
+    {
+        public HinhBinhHanh(int soCanh) : base(soCanh)
+        {
+            SoCanh = soCanh;
+        }
+
+        override public void Xuat()
+        {
+            Console.WriteLine("Co 2 cap canh doi song song bang nhau, 2 goc doi bang nhau va 2 duong cheo cat nhau tai trung diem cua hinh.");
+        }
+    }
     class Program
     {
-        public struct sinhvien
-        {
-            public string maso111;
-            public string hoten111;
-            public double diemtoan111;
-            public gioitinh gioitinh111;
-            public double diemly111;
-            public double diemhoa111;
-        }
-        public enum gioitinh
-        {
-            Nam,
-            Nu
-        };
-        public enum hocluc
-        {
-            Yeu,
-            TrungBinh,
-            Kha,
-            Gioi
-        };
-        public static void nhapDS(ref sinhvien[] sv, int n)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                Console.WriteLine("Nhap sinh vien thu " + (i + 1));
-                Console.Write("Ho ten: ");
-                string ht111 = Console.ReadLine();
-                Console.Write("Ma sinh vien: ");
-                string msv111 = Console.ReadLine();
-                Console.Write("Gioi tinh (0: nam  1: nu): ");
-                int gt111 = int.Parse(Console.ReadLine());
-                Console.Write("Diem toan: ");
-                double dt111 = double.Parse(Console.ReadLine());
-                Console.Write("Diem ly: ");
-                double dl111 = double.Parse(Console.ReadLine());
-                Console.Write("Diem hoa: ");
-                double dh111 = double.Parse(Console.ReadLine());
-
-
-                sv[i].hoten111 = ht111;
-                sv[i].maso111 = msv111;
-                if (gt111 == 0)
-                    sv[i].gioitinh111 = gioitinh.Nam;
-                else
-                    sv[i].gioitinh111 = gioitinh.Nu;
-                sv[i].diemtoan111 = dt111;
-                sv[i].diemly111 = dl111;
-                sv[i].diemhoa111 = dh111;
-
-            }
-        }
-        public static void xuatDS(sinhvien[] sv, int n)
-        {
-            int i = 1;
-            foreach (sinhvien item111 in sv)
-            {
-                Console.WriteLine("----- " + i + " -----");
-                Console.WriteLine("Ho ten: " + item111.hoten111);
-                Console.WriteLine("Ma sinh vien: " + item111.maso111);
-                int gt = (int)item111.gioitinh111;
-                if (gt == 0)
-                    Console.WriteLine("Gioi tinh: Nam");
-                else
-                    Console.WriteLine("Gioi tinh: Nữ");
-                Console.WriteLine("Diem toan: " + item111.diemtoan111);
-                Console.WriteLine("Diem ly: " + item111.diemly111);
-                Console.WriteLine("Diem hoa: " + item111.diemhoa111);
-                int hocluc = (int)diemTB(item111.diemtoan111, item111.diemly111, item111.diemhoa111);
-                switch (hocluc)
-                {
-                    case 0:
-                        Console.WriteLine("Hoc luc: Yeu");
-                        break;
-                    case 1:
-                        Console.WriteLine("Hoc luc: Trung Binh");
-                        break;
-                    case 2:
-                        Console.WriteLine("Hoc luc: Kha");
-                        break;
-                    case 3:
-                        Console.WriteLine("Hoc luc:Gioi");
-                        break;
-
-                }
-
-                i++;
-            }
-        }
-        public static hocluc diemTB(double toan, double ly, double hoa)
-        {
-            double dtb = (toan + ly + hoa) / 3;
-            if (dtb <= 4)
-                return hocluc.Yeu;
-            else if (dtb <= 5)
-                return hocluc.TrungBinh;
-            else if (dtb <= 7)
-                return hocluc.Kha;
-            else
-                return hocluc.Gioi;
-        }
-        public static void timKiem(sinhvien[] sv, int n, string ht)
-        {
-            foreach (sinhvien item117 in sv)
-            {
-                if (item117.hoten111 == ht)
-                {
-                    Console.WriteLine("Ho ten: " + item117.hoten111);
-                    Console.WriteLine("Ma sinh vien: " + item117.maso111);
-                    int gt = (int)item117.gioitinh111;
-                    if (gt == 0)
-                        Console.WriteLine("Gioi tinh: Nam");
-                    else
-                        Console.WriteLine("Gioi tinh: Nữ");
-                    Console.WriteLine("Diem toan: " + item117.diemtoan111);
-                    Console.WriteLine("Diem ly: " + item117.diemly111);
-                    Console.WriteLine("Diem hoa: " + item117.diemhoa111);
-                    int hocluc = (int)diemTB(item117.diemtoan111, item117.diemly111, item117.diemhoa111);
-                    switch (hocluc)
-                    {
-                        case 0:
-                            Console.WriteLine("Hoc luc: Yeu");
-                            break;
-                        case 1:
-                            Console.WriteLine("Hoc luc: Trung Binh");
-                            break;
-                        case 2:
-                            Console.WriteLine("Hoc luc: Kha");
-                            break;
-                        case 3:
-                            Console.WriteLine("Hoc luc:Gioi");
-                            break;
-
-                    }
-                }
-            }
-        }
         static void Main(string[] args)
         {
-            Console.Write("Nhap so sinh vien: ");
-            int n111 = int.Parse(Console.ReadLine());
-            sinhvien[] sv111 = new sinhvien[n111];
-            nhapDS(ref sv111, n111);
-            Console.WriteLine("\n\n==========DANH SACH SINH VIEN==========");
-            xuatDS(sv111, n111);
+            Console.WriteLine("1. Tam giac");
+            Console.WriteLine("2. Tu giac");
+            Console.WriteLine("3. Hinh binh hanh");
+            Console.WriteLine("4. Hinh chu nhat");
+            Console.WriteLine("5. Hinh vuong");
+            Console.Write("Chon: ");
+            int hinh = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("\n==========TIM KIEM SINH VIEN==========");
-            Console.Write("Nhap ten sinh vien: ");
-            string ht111 = Console.ReadLine();
-            timKiem(sv111, n111, ht111);
+            if(hinh == 1)
+            {
+                DaGiac tg = new TamGiac(3);
+                tg.Xuat();
+            } else if(hinh == 2)
+            {
+                DaGiac tg = new HinhTuGiac(4);
+                tg.Xuat();
+            } else if (hinh == 3)
+            {
+                DaGiac tg = new HinhBinhHanh(4);
+                tg.Xuat();
+            } else if (hinh == 4)
+            {
+                DaGiac tg = new HinhChuNhat(4);
+                tg.Xuat();
+            } else if (hinh == 5)
+            {
+                DaGiac tg = new HinhVuong(4);
+                tg.Xuat();
+            } else
+            {
+                Console.WriteLine("Lua chon khong hop le!");
+            }
 
             Console.ReadLine();
         }
-
-
     }
 }
